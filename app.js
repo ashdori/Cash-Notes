62b0fb78c5f22733a44f6fd8c7611f1f53635af1
asep hudori <asep.hudori532@gmail.com>
2024-08-21 13:18:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+app.use('/', (req, res) => {
+  try {
+    let welcomeMessage = {
+      message: 'Welcome to Cash Notes',
+    };
+    res.json(welcomeMessage);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
---
+++ models/notes.models.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+    amount: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+}, { timestamps: true });
+
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Note;
