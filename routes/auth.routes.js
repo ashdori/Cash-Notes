76186416cc6cccf533a40eabba073ef4b97a6ab7
asep hudori <asep.hudori532@gmail.com>
2024-08-28 20:42:10 +0700

    add refresh token routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 const Router = require('express')
-const { register, login } = require('../controllers/auth.controllers')
+const { register, login, refreshToken } = require('../controllers/auth.controllers')
 
 const authRouter = Router()
 
 authRouter.post("/register", register)
 authRouter.post("/login", login)
+authRouter.get("/refreshToken",refreshToken)
 
 module.exports = authRouter;
