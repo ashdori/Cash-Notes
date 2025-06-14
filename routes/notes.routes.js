cb5b1dc5e141bf88a501406926706ead844b1327
ashdori <asep.hudori532@gmail.com>
2025-06-14 14:06:20 +0700

    fix rotes

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -1,6 +1,8 @@
 const Router = require('express');
 const {
   addNotes,
+  getNotesById,
+  getAllNotes,
   updateNotes,
   removeNotes,
 } = require('../controllers/notes.controllers');
@@ -8,7 +10,9 @@ const {
 const notesRouter = Router();
 
 notesRouter.post('/create', addNotes);
-notesRouter.put('/update', updateNotes);
-notesRouter.delete('/remove', removeNotes);
+notesRouter.get('/:id', getNotesById)
+notesRouter.get('/', getAllNotes)
+notesRouter.put('/:id', updateNotes);
+notesRouter.delete('/:id', removeNotes);
 
 module.exports = notesRouter;
